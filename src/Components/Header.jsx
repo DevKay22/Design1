a4c16ee39552f2adabcb29f67e901b321d151a62
Kayode <drkay4sure@gmail.com>
2025-03-27 10:31:37 +0100

    setup successful

---
+++ src/Components/Header.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import '../Styles/Header.css';
+
+const Header = () => {
+  return (
+    <div>
+        <div>
+            <div>S</div>
+            <div>W</div>
+            <div>A</div>
+        </div>
+        <div></div>
+    </div>
+  )
+}
+
+export default Header
