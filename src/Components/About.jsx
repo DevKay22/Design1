f3d9c3625bb7cb6f07e7be9ec3931cd5e03ba870
Kayode <drkay4sure@gmail.com>
2025-03-28 19:46:45 +0100

    images added

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import '../Styles/About.css';
+import people from '../assets/Images/blog21.jpg';
+import person from '../assets/Images/blog17.jpg';
 
 const About = () => {
   return (
@@ -40,12 +42,58 @@ const About = () => {
                     </div>
                 </div>
 
-                <div classname='abtListMore'>
+                <div id='btc'>
                     <button>Read More</button>
+                    
                 </div>
             </div>
-            <div>hi</div>
+            <div className='abtItems1b'>
+                <img src={people} alt="" />
+            </div>
         </div>
+
+        <div className='abtItems'>
+            <div className='abtItem1'>
+                <h1>
+                    Association for Society
+                </h1>
+
+                <p>
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Suscipit laudantium sequi
+                     repellat, error quis assumenda aliquid, 
+                     nventore 
+                </p>
+
+                <div className='abtItemsList'>
+                    <div>
+                        <ul>
+                            <li>Mending lives</li>
+                            <li>Helping people</li>
+                            <li>Healing hurts</li>
+                            <li>Restoring peace</li>
+                        </ul>
+                    </div>
+
+                    <div>
+                        <ul>
+                            <li>lorem ipsim</li>
+                            <li>sit met</li>
+                            <li>doloe hela</li>
+                            <li>ipsum dolor</li>
+                        </ul>
+                    </div>
+                </div>
+
+                <div id='btc'>
+                    <button>Read More</button>
+                    
+                </div>
+            </div>
+            <div className='abtItems1b'>
+                <img src={person} alt="" />
+            </div>
+        </div>
+
     </div>
   )
 }
