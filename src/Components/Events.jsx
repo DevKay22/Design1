faad5af72f01e9080633d4490f11e0f757241b4e
Kayode <drkay4sure@gmail.com>
2025-03-31 23:12:35 +0100

    done with events

--- src/Components/Events.jsx
+++ src/Components/Events.jsx
@@ -1,11 +1,92 @@
 import React from 'react'
-import '../Styles/Events.css'
+import '../Styles/Events.css';
+import person1 from '../assets/Images/person1.jpeg';
+import person2 from '../assets/Images/blog16.jpg'
+import { FaCalendarAlt } from "react-icons/fa";
+import { IoPerson } from "react-icons/io5";
+
 const Events = () => {
   return (
-    <div className='upcoming'>
-        <h1>
+    <div >
+      <div className='upcoming'>
+      <h1>
             UPCOMING EVENTS
         </h1>
+      </div>
+       
+
+        <div className='theEvents'>
+          {/* event1 */}
+          <div  className='event1'>
+              <div className='person1'>
+                <img src={person1} alt=""  />
+              </div>
+
+              <h1>
+
+                INTERNATIONAL MEET IN 2024
+              </h1>
+
+              <div className='iconCont'>
+                <div>
+                <p><span className='iccon'><FaCalendarAlt /></span> 26th March, 2024 </p>
+                </div>
+                <div>
+                    <p>
+                      <span className='iccon'><IoPerson /></span> John
+                    </p>
+                </div>
+                
+                  
+              </div>
+
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil, ullam maiores. Quisquam amet, ex eaque, quidem consequuntur incidunt assumenda unde reprehenderit similique quaerat animi, pariatur eius? Voluptates voluptate dolor illum.
+              </p>
+
+              <div>
+                <button>
+                  Learn More
+                </button>
+              </div>
+          </div>
+
+            {/* event2 */}
+            <div  className='event1'>
+              <div className='person1'>
+                <img src={person2} alt=""  />
+              </div>
+
+              <h1>
+
+                PUBLIC WELFARE MEET IN 2024
+              </h1>
+
+              <div className='iconCont'>
+                <div>
+                <p><span className='iccon'><FaCalendarAlt /></span> 26th March, 2024 </p>
+                </div>
+                <div>
+                    <p>
+                      <span className='iccon'><IoPerson /></span> John
+                    </p>
+                </div>
+                
+                  
+              </div>
+
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil, ullam maiores. Quisquam amet, ex eaque, quidem consequuntur incidunt assumenda unde reprehenderit similique quaerat animi, pariatur eius? Voluptates voluptate dolor illum.
+              </p>
+
+              <div>
+                <button>
+                  Learn More
+                </button>
+              </div>
+          </div>
+
+        </div>
     </div>
   )
 }
